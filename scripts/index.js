ca3f72e9c7939de9a28fca4f9e39f00831320534
mtafunai <roy.tafunai10@gmail.com>
2026-05-17 19:13:46 +1200

    file updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
 
-function toggleMenu() {
-  document.getElementById("nav").classList.toggle("show");
-}
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click", () => {
+nav.classList.toggle("show");
+});
+
 
 
 document.getElementById("year").textContent = new Date().getFullYear();
-
 document.getElementById("modified").textContent =
-document.lastModified;
+"Last Modified: " + document.lastModified;
 
 
 
 const courses = [
 { code: "WDD130", name: "Web Fundamentals", credits: 2, completed: true },
-{ code: "WDD231", name: "Frontend Development", credits: 2, completed: false },
+{ code: "WDD231", name: "Frontend Dev", credits: 2, completed: false },
 { code: "CSE110", name: "Programming Basics", credits: 2, completed: true },
-{ code: "CSE111", name: "Functions & Logic", credits: 2, completed: false },
-{ code: "CSE210", name: "Programming with Classes", credits: 3, completed: false }
+{ code: "CSE111", name: "Functions", credits: 3, completed: false }
 ];
 
 const container = document.getElementById("courseContainer");
-const creditsDisplay = document.getElementById("credits");
+const credits = document.getElementById("credits");
 
 
 
@@ -28,44 +30,45 @@ function displayCourses(list) {
 container.innerHTML = "";
 
 list.forEach(course => {
-    const card = document.createElement("div");
-    card.classList.add("card");
+    const div = document.createElement("div");
+    div.classList.add("card");
 
-    if (course.completed) {
-    card.classList.add("completed");
-    } else {
-    card.classList.add("incomplete");
-    }
+    div.classList.add(course.completed ? "completed" : "incomplete");
 
-    card.innerHTML = `
+    div.innerHTML = `
     <h3>${course.code}</h3>
     <p>${course.name}</p>
     <p>${course.credits} Credits</p>
     `;
 
-    container.appendChild(card);
+    container.appendChild(div);
 });
 
 
-const totalCredits = list.reduce((sum, c) => sum + c.credits, 0);
-creditsDisplay.textContent = totalCredits;
+const total = list.reduce((sum, c) => sum + c.credits, 0);
+credits.textContent = total;
 }
 
 
 
-function filterCourses(type) {
-let filtered = courses;
+document.querySelectorAll(".filters button").forEach(btn => {
+btn.addEventListener("click", () => {
+    const type = btn.dataset.filter;
 
-if (type === "completed") {
+    let filtered = courses;
+
+    if (type === "completed") {
     filtered = courses.filter(c => c.completed);
-}
+    }
 
-if (type === "incomplete") {
+    if (type === "incomplete") {
     filtered = courses.filter(c => !c.completed);
-}
+    }
 
-displayCourses(filtered);
-}
+    displayCourses(filtered);
+});
+});
 
 
+// INIT
 displayCourses(courses);
