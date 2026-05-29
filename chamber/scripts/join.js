5e8f886da181a29719796cf53a672f96e9aaf94c
mtafunai <roy.tafunai10@gmail.com>
2026-05-30 05:00:06 +1200

    updated

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,12 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.getElementById("timestamp").value = new Date().toISOString();
 
+const modalLinks = document.querySelectorAll(".open-modal");
 
-    document.querySelector("#timestamp").value = new Date().toISOString();
+modalLinks.forEach(link => {
+
+    link.addEventListener("click", (event) => {
+
+        event.preventDefault();
+
+        const modalId = link.dataset.modal;
+
+        document.getElementById(modalId).showModal();
+
+    });
 
 });
 
+const closeButtons = document.querySelectorAll(".close-modal");
 
-function openModal(id) {
-    document.querySelectorAll(".modal").forEach(m => m.style.display = "none");
-    document.getElementById(id).style.display = "block";
-}
+closeButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        button.closest("dialog").close();
+
+    });
+
+});
+```
